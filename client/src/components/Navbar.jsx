e6d6fe2aee6d4893f8b1ef8735af4bb5a4377d4e
Shrikant Ingle <229553626+shrikantingle3319@users.noreply.github.com>
2026-05-12 00:36:02 +0530

    Add files via upload

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3">
+      <div className="container">
+
+        <a className="navbar-brand fw-bold" href="/">
+          Auto Insurance
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav ms-auto">
+
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="/quotes">
+                Quotes
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">
+                Contact
+              </a>
+            </li>
+
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
